dd7453f9cd44da4513b33dfa46230230f1e4b0fb
Karim Raz <karim.razok@gmail.com>
2020-01-12 21:29:35 +0100

    (admin) gui admin finished

--- src/Screens/Admin/AdminScreena.js
+++ src/Screens/Admin/AdminScreena.js
@@ -1,37 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   View,
-  StyleSheet, 
-  SafeAreaView, 
+  StyleSheet,
+  FlatList,
+  Alert,
+  TouchableOpacity, 
 } from "react-native";
 
-import { Text, Button,Input } from "react-native-elements"; 
+import { Text, Button } from "react-native-elements"; 
 
-
-export default class AddAdmin extends Component {
-  render() {
-    return ( 
-      <View style={styles.container}>
+  
+export default class AdminScreen extends Component {
+    render() {
+      return (
+    <View style={styles.container}>
       <View style={styles.body}>
-        <Text style={{ alignContent: "center", marginBottom: 30 }} h4>
+        <Text style={{ alignContent: "center", marginBottom: 30 }} h3>
           Administrateur
-        </Text> 
+        </Text>
+        <Text>Listes des Administrateurs </Text>
         <View style={{ width: "100%", flex: 4  }}>
           <View
             style={{
               borderBottomColor: "black",
               borderBottomWidth: 1 ,
             }}
-          /> 
-          <View style={{ flex: 1, paddingTop: 10 }}>
-          <SafeAreaView style ={{paddingBottom : 20}}>
-          <Input placeholder=" UserAdmin"/>
-            </SafeAreaView>  
-            <Button title="Ajouter" 
-             onPress={() => {
-              this.props.navigation.navigate("Administrateurs");
-            }}
+          />
+          <View style={{ flex: 5}}>
+            <FlatList
+              data={[
+                { key: "A1" },
+                { key: "A2" },
+                { key: "A3" },
+                { key: "A4" },
+                { key: "A11" },
+                { key: "A12" },
+                { key: "A13" },
+                { key: "A14" },
+                { key: "A21" },
+                { key: "A22" },
+                { key: "A23" },
+                { key: "A24" }
+              ]}
+              renderItem={({ item }) => {
+                return (
+                  <TouchableOpacity onPress={() => Alert.alert("Clicked !")}>
+                    <Text style={styles.item}> {item.key} </Text>
+                  </TouchableOpacity>
+                );
+              }}
             />
+          </View>
+          <View style={{ flex: 1, paddingTop: 10 }}>
+            <Button title="Ajouter Administrateur"/>
           </View>
         </View>
       </View>
@@ -43,13 +64,12 @@ export default class AddAdmin extends Component {
           justifyContent: "center"
         }}
       >
+ 
       </View>
     </View>
-    )
-  }
+  );
 }
-
-
+}
 
 const styles = StyleSheet.create({
   container: {
--- src/Screens/Admin/studentScreen.js
+++ src/Screens/Admin/studentScreen.js
@@ -4,78 +4,78 @@ import {
   StyleSheet,
   FlatList,
   Alert,
-  TouchableOpacity, 
+  TouchableOpacity
 } from "react-native";
 
-import { Text, Button } from "react-native-elements"; 
+import { Text, Button } from "react-native-elements";
 
-
-export default function ClassesScreen() {  
-  return (
-    <View style={styles.container}>
-      <View style={styles.body}>
-        <Text style={{ alignContent: "center", marginBottom: 30 }} h3>
-          Classes
-        </Text>
-        <Text>Listes des Classes </Text>
-        <View style={{ width: "100%", flex: 4  }}>
-          <View
-            style={{
-              borderBottomColor: "black",
-              borderBottomWidth: 1 ,
-            }}
-          />
-          <View style={{ flex: 5}}>
-            <FlatList
-              data={[
-                { key: "A1" },
-                { key: "A2" },
-                { key: "A3" },
-                { key: "A4" },
-                { key: "A11" },
-                { key: "A12" },
-                { key: "A13" },
-                { key: "A14" },
-                { key: "A21" },
-                { key: "A22" },
-                { key: "A23" },
-                { key: "A24" }
-              ]}
-              renderItem={({ item }) => {
-                return (
-                  <TouchableOpacity onPress={() => Alert.alert("Clicked !")}>
-                    <Text style={styles.item}> {item.key} </Text>
-                  </TouchableOpacity>
-                );
+export default class StudentScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <Text style={{ alignContent: "center", marginBottom: 30 }} h3>
+            Etudiant
+          </Text>
+          <Text>Listes des étudiants </Text>
+          <View style={{ width: "100%", flex: 4 }}>
+            <View
+              style={{
+                borderBottomColor: "black",
+                borderBottomWidth: 1
               }}
             />
-          </View>
-          <View style={{ flex: 1, paddingTop: 10 }}>
-            <Button title="Ajouter Administrateur"/>
+            <View style={{ flex: 5 }}>
+              <FlatList
+                data={[
+                  { key: "E1" },
+                  { key: "E2" },
+                  { key: "E3" },
+                  { key: "E4" },
+                  { key: "E11" },
+                  { key: "E12" },
+                  { key: "E13" },
+                  { key: "E14" },
+                  { key: "E21" },
+                  { key: "E22" },
+                  { key: "E23" },
+                  { key: "E24" }
+                ]}
+                renderItem={({ item }) => {
+                  return (
+                    <TouchableOpacity onPress={() => Alert.alert("Clicked !")}>
+                      <Text style={styles.item}> {item.key} </Text>
+                    </TouchableOpacity>
+                  );
+                }}
+              />
+            </View>
+            <View style={{ flex: 1, paddingTop: 10 }}>
+              <Button
+                title="Ajouter"
+                onPress={() => this.props.navigation.navigate("addStudent")}
+              />
+            </View>
           </View>
         </View>
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            alignItems: "flex-end",
+            justifyContent: "center"
+          }}
+        ></View>
       </View>
-      <View
-        style={{
-          flexDirection: "row",
-          width: "100%",
-          alignItems: "flex-end",
-          justifyContent: "center"
-        }}
-      >
-
-
- 
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    backgroundColor :"#fff",
+    backgroundColor: "#fff"
   },
   body: {
     flex: 1,
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
     margin: 20,
     backgroundColor: "#fff",
     display: "flex",
-    alignItems: "center",
+    alignItems: "center"
   },
   item: {
     borderBottomWidth: 1,
@@ -104,14 +104,13 @@ const styles = StyleSheet.create({
   },
   text: { color: "#0275d8" },
   textActive: { color: "#fff" },
-  Active: { 
+  Active: {
     borderWidth: 1,
     borderColor: "#0275d8",
     width: "34%",
     paddingTop: 20,
     paddingBottom: 20,
-    backgroundColor: "#0275d8", 
-    alignItems: "center" ,
-  
+    backgroundColor: "#0275d8",
+    alignItems: "center"
   }
 });
